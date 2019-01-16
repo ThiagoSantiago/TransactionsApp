6fc6880c1036dc4a5dcad1e87e1dd5887a6be8c3
Thiago Santiago <>
2019-01-16 11:05:45 -0200

    Initial commit

---
+++ Transactions/Transactions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Transactions
+//
+//  Created by Thiago Santiago on 1/16/19.
+//  Copyright © 2019 Thiago Santiago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
